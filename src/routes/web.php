cafe9463992a0ef3c26cc0078a218c263a5a44a9
佐藤野和 <nowasato@sukefujinowanoMacBook-Air.local>
2026-02-01 16:06:09 +0900

    新規登録バックエンド実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,19 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\SignupController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+
+// login（未ログインのみ表示したいなら guest 付けてもOK）
+Route::get('/login', [LoginController::class, 'showLoginForm'])
+    ->name('login');
+
+// signup：ログイン済みなら login にリダイレクト
+Route::get('/signup', [SignupController::class, 'create'])
+    ->middleware('guest')
+    ->name('signup.create');
+
+Route::post('/signup', [SignupController::class, 'store'])
+    ->middleware('guest')
+    ->name('signup.store');
